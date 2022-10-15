226b381889188653db68c052946949b273605062
O B thuis <olaf.booij@xs4all.nl>
2022-10-15 16:13:37 +0200

    even more ready

--- test/test_xor_arch_forward.cpp
+++ test/test_xor_arch_forward.cpp
@@ -6,8 +6,6 @@
 namespace resp
 {
 
-  using neuron_ptr = std::shared_ptr<neuron>;
-
   auto connect_neurons(auto pre, auto post, auto random_weight, auto random_gen)
   {
     for(auto delay_i = 16; delay_i--;)
@@ -24,7 +22,7 @@ namespace resp
 
   auto create_layer(std::vector<std::string>&& keys)
   {
-    std::vector<neuron_ptr> layer;
+    std::vector<std::shared_ptr<neuron>> layer;
     for(auto key: keys)
       layer.emplace_back(make_shared<neuron>(key));
     return layer;
@@ -40,22 +38,19 @@ int main()
   auto input_layer = create_layer({"input 1", "input 2", "bias"});
   auto hidden_layer = create_layer({"hidden 1", "hidden 2", "hidden 3", "hidden 4", "hidden 5"});
   auto output_layer = create_layer({"output"});
-  std::vector<neuron_ptr> all_neurons;
-  for(auto n: input_layer) all_neurons.emplace_back(n);
-  for(auto n: hidden_layer) all_neurons.emplace_back(n);
-  for(auto n: output_layer) all_neurons.emplace_back(n);
 
   connect_layers(input_layer, hidden_layer, -.5, 1., random_gen);
-  // create synapses with only positive weights for 4 hidden neurons
-  //connect_layers(std::vector<neuron_ptr>(hidden_layer.begin(), hidden_layer.end() - 1), output_layer, 0., 1., random_gen);
   {
+    // create synapses with only positive weights for 4 hidden neurons
     std::uniform_real_distribution<> random_weight(0., 1.);
     for(auto pre_it = hidden_layer.begin(); pre_it != hidden_layer.end() - 1; ++pre_it)
       connect_neurons(*pre_it, output_layer.at(0), random_weight, random_gen);
   }
-  // and with only negative weights for the last hidden neuaron
-  std::uniform_real_distribution<> random_weight(-.5, 0.);
-  connect_neurons(hidden_layer.back(), output_layer.front(), random_weight, random_gen);
+  {
+    // and with only negative weights for the last hidden neuaron
+    std::uniform_real_distribution<> random_weight(-.5, 0.);
+    connect_neurons(hidden_layer.back(), output_layer.front(), random_weight, random_gen);
+  }
 
   // first XOR pattern
   input_layer.at(0)->fire(0.);
