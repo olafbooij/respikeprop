3d9f0355c1f76bcd7e439afa48eb6583305a2205
O B thuis <olaf.booij@xs4all.nl>
2022-10-16 10:15:31 +0200

    should have passed random gen by reference. Fixture change!

--- test/test_xor_arch_forward.cpp
+++ test/test_xor_arch_forward.cpp
@@ -6,13 +6,13 @@
 namespace resp
 {
 
-  auto connect_neurons(auto pre, auto post, auto random_weight, auto random_gen)
+  auto connect_neurons(auto& pre, auto& post, auto& random_weight, auto& random_gen)
   {
     for(auto delay_i = 16; delay_i--;)
       post->incoming_synapses.emplace_back(pre, random_weight(random_gen), delay_i + 1.0);
   };
 
-  auto connect_layers(auto pre_layer, auto post_layer, double min_weight, double max_weight, auto random_gen)
+  auto connect_layers(auto& pre_layer, auto& post_layer, double min_weight, double max_weight, auto& random_gen)
   {
     std::uniform_real_distribution<> random_weight(min_weight, max_weight);
     for(auto pre: pre_layer)
@@ -70,7 +70,7 @@ int main()
   assert(! output_layer.at(0)->spikes.empty());
 
   // fixture
-  assert(fabs(output_layer.at(0)->spikes.at(5) - 19.7) < 0.2);
+  assert(fabs(output_layer.at(0)->spikes.at(5) - 17.3) < 0.2);
 
   //for(auto time: input_layer.at(0)->spikes)
   //  std::cout << time << std::endl;
