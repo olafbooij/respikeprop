1d4e74cb1b19a3df5812cc08281036d6d27e4cee
O B thuis <olaf.booij@xs4all.nl>
2022-11-29 15:30:15 +0100

    code to own function

--- test/n-mnist.cpp
+++ test/n-mnist.cpp
@@ -3,10 +3,8 @@
 #include<vector>
 #include<fstream>
 
-int main()
+namespace resp
 {
-  using namespace std;
-  ifstream file("datasets/n-mnist/Train/0/00002.bin", std::ios::binary);
   struct Event
   {
     uint8_t x;
@@ -15,20 +13,33 @@ int main()
     int timestamp;
   };
 
-  std::vector<Event> events;
-	while(file.good())
+  auto load_spike_pattern(auto&& file)
   {
-    Event event;
-    event.x = file.get();
-    event.y = file.get();
-    uint8_t c = file.get();
-    event.polarity = c >> 7;
-    event.timestamp = (c & 127) << 16;
-    event.timestamp += file.get() << 8;
-    event.timestamp += file.get();
-    if(file.good())
-      events.emplace_back(event);
+    std::vector<Event> events;
+    while(file.good())
+    {
+      Event event;
+      event.x = file.get() + 1;
+      event.y = file.get() + 1;
+      uint8_t c = file.get();
+      event.polarity = c >> 7;
+      event.timestamp = (c & 127) << 16;
+      event.timestamp += file.get() << 8;
+      event.timestamp += file.get();
+      if(file.good())
+        if(event.x < 29 && event.y < 29)  // about 5% of the events seem to be outside the given 28x28 frame, dropping them
+          events.emplace_back(event);
+    }
+    return events;
   }
+}
+
+int main()
+{
+  using namespace std;
+  using namespace resp;
+  auto events = load_spike_pattern(std::ifstream("datasets/n-mnist/Train/0/00002.bin", std::ios::binary));
+
   for(auto event: events)
     std::cout << static_cast<int>(event.x) << " " << static_cast<int>(event.y) << " " << event.polarity << " " << event.timestamp << std::endl;
 
