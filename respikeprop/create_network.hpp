678bdeea22a25ccc19a75f0a6532dba7384707dd
olaf booij in mist <olafbooij@bestprima.nl>
2022-12-15 10:42:17 +0100

    stray ;'s

--- respikeprop/create_network.hpp
+++ respikeprop/create_network.hpp
@@ -7,14 +7,14 @@ namespace resp
     auto& incoming_connection = post.incoming_connections.emplace_back(&pre);
     for(auto delay_i = 16; delay_i--;)
       incoming_connection.synapses.emplace_back(.0, delay_i + 1.0 + 1e-10, 0.);
-  };
+  }
 
   void connect_layers(auto& pre_layer, auto& post_layer)
   {
     for(auto& pre: pre_layer)
       for(auto& post: post_layer)
         connect_neurons(pre, post);
-  };
+  }
 
   auto create_layer(const std::vector<std::string>&& keys)
   {
@@ -22,7 +22,7 @@ namespace resp
     for(const auto& key: keys)
       layer.emplace_back(key);
     return layer;
-  };
+  }
 
   void clear(auto& network)
   {
